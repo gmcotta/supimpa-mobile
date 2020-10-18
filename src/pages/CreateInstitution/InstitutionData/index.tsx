b2de4f762ac7f8c6f1d9cc1e4f3a117aec462cd8
Gustavo Cotta <gmcotta34@gmail.com>
2020-10-18 16:31:37 -0300

    Styling institution data screen

--- src/pages/CreateInstitution/InstitutionData/index.tsx
+++ src/pages/CreateInstitution/InstitutionData/index.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Switch, KeyboardAvoidingView, Platform } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const InstitutionData: React.FC = () => {
+import {
+  Container,
+  Title,
+  Label,
+  CustomInput,
+  MultilineCustomInput,
+  ImagesInput,
+  SwitchContainer,
+  NextButton,
+  NextButtonText,
+} from './styles';
+
+const OrphanageData: React.FC = () => {
   return (
-    <View>
-      <Text>Dados da instituição</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+      style={{ flex: 1 }}
+    >
+      <Container contentContainerStyle={{ padding: 24 }}>
+        <Title>Dados</Title>
+
+        <Label>Nome</Label>
+        <CustomInput />
+
+        <Label>Sobre</Label>
+        <MultilineCustomInput textAlignVertical="top" />
+
+        <Label>Whatsapp</Label>
+        <CustomInput />
+
+        <Label>Fotos</Label>
+        <ImagesInput onPress={() => {}}>
+          <Feather name="plus" size={24} color="#15B6D6" />
+        </ImagesInput>
+
+        <Title>Visitação</Title>
+
+        <Label>Instruções</Label>
+        <MultilineCustomInput textAlignVertical="top" />
+
+        <Label>Horário de visitas</Label>
+        <CustomInput />
+
+        <SwitchContainer>
+          <Label>Atende final de semana?</Label>
+          <Switch
+            thumbColor="#fff"
+            trackColor={{ false: '#ccc', true: '#39CC83' }}
+          />
+        </SwitchContainer>
+
+        <NextButton onPress={() => {}}>
+          <NextButtonText>Cadastrar</NextButtonText>
+        </NextButton>
+      </Container>
+    </KeyboardAvoidingView>
   );
 };
 
-export default InstitutionData;
+export default OrphanageData;
---
+++ src/pages/CreateInstitution/InstitutionData/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  color: #5c8599;
+  font-size: 24px;
+  font-family: 'Montserrat_700Bold';
+  margin-bottom: 32px;
+  padding-bottom: 24px;
+  border-bottom-width: 0.8px;
+  border-bottom-color: #d3e2e6;
+`;
+
+export const Label = styled.Text`
+  color: #8fa7b3;
+  font-family: 'Montserrat_600SemiBold';
+  margin-bottom: 8px;
+`;
+
+export const CustomInput = styled.TextInput`
+  background-color: #fff;
+  border-width: 1.4px;
+  border-color: #d3e2e6;
+  border-radius: 20px;
+  height: 56px;
+  padding: 18px 24px;
+  margin-bottom: 16px;
+`;
+
+export const MultilineCustomInput = styled(CustomInput)`
+  height: 110px;
+`;
+
+export const ImagesInput = styled.TouchableOpacity`
+  background-color: rgba(255, 255, 255, 0.5);
+  border-style: dashed;
+  border-color: #96d2f0;
+  border-width: 1.4px;
+  border-radius: 20px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 32;
+`;
+
+export const SwitchContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+`;
+
+export const NextButton = styled(RectButton)`
+  background-color: #15c3d6;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  margin-top: 32px;
+`;
+
+export const NextButtonText = styled.Text`
+  font-family: 'Montserrat_800ExtraBold';
+  font-size: 16px;
+  color: #fff;
+`;
