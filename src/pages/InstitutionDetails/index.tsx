354681a5f2f94ccdc94cb44e0de381b499b268aa
Gustavo Cotta <gmcotta34@gmail.com>
2020-10-18 15:41:22 -0300

    Styling institution details page

--- src/pages/InstitutionDetails/index.tsx
+++ src/pages/InstitutionDetails/index.tsx
@@ -1,11 +1,114 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
+import { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather, FontAwesome } from '@expo/vector-icons';
+
+import mapMarkerImg from '../../assets/images/grandma.png';
+
+import {
+  Container,
+  ImagesContainer,
+  CustomImage,
+  DetailsContainer,
+  Title,
+  Description,
+  MapContainer,
+  Map,
+  RoutesContainer,
+  RoutesText,
+  Separator,
+  ScheduleContainer,
+  ScheduleItemBlue,
+  ScheduleTextBlue,
+  ScheduleItemGreen,
+  ScheduleTextGreen,
+  ContactButton,
+  ContactButtonText,
+} from './styles';
 
 const InstitutionDetails: React.FC = () => {
   return (
-    <View>
-      <Text>Detalhes</Text>
-    </View>
+    <Container>
+      <ImagesContainer>
+        <ScrollView horizontal pagingEnabled>
+          <CustomImage
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <CustomImage
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+          <CustomImage
+            source={{
+              uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg',
+            }}
+          />
+        </ScrollView>
+      </ImagesContainer>
+
+      <DetailsContainer>
+        <Title>Orf. Esperança</Title>
+        <Description>
+          Presta assistência a crianças de 06 a 15 anos que se encontre em
+          situação de risco e/ou vulnerabilidade social.
+        </Description>
+
+        <MapContainer>
+          <Map
+            initialRegion={{
+              latitude: -27.2092052,
+              longitude: -49.6401092,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }}
+            zoomEnabled={false}
+            pitchEnabled={false}
+            scrollEnabled={false}
+            rotateEnabled={false}
+            provider={PROVIDER_GOOGLE}
+          >
+            <Marker
+              icon={mapMarkerImg}
+              coordinate={{
+                latitude: -27.2092052,
+                longitude: -49.6401092,
+              }}
+            />
+          </Map>
+
+          <RoutesContainer>
+            <RoutesText>Ver rotas no Google Maps</RoutesText>
+          </RoutesContainer>
+        </MapContainer>
+
+        <Separator />
+
+        <Title>Instruções para visita</Title>
+        <Description>
+          Venha como se sentir a vontade e traga muito amor e paciência para
+          dar.
+        </Description>
+
+        <ScheduleContainer>
+          <ScheduleItemBlue>
+            <Feather name="clock" size={40} color="#2AB5D1" />
+            <ScheduleTextBlue>Segunda à Sexta 8h às 18h</ScheduleTextBlue>
+          </ScheduleItemBlue>
+          <ScheduleItemGreen>
+            <Feather name="info" size={40} color="#39CC83" />
+            <ScheduleTextGreen>Atendemos fim de semana</ScheduleTextGreen>
+          </ScheduleItemGreen>
+        </ScheduleContainer>
+
+        <ContactButton onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={24} color="#FFF" />
+          <ContactButtonText>Entrar em contato</ContactButtonText>
+        </ContactButton>
+      </DetailsContainer>
+    </Container>
   );
 };
 
---
+++ src/pages/InstitutionDetails/styles.ts
@@ -0,0 +1,133 @@
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+export const ImagesContainer = styled.View`
+  height: 240px;
+`;
+
+export const CustomImage = styled.Image`
+  width: ${`${Dimensions.get('window').width}px`};
+`;
+
+export const DetailsContainer = styled.View`
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  color: #4d6f80;
+  font-size: 30px;
+  font-family: 'Montserrat_700Bold';
+`;
+
+export const Description = styled.Text`
+  font-family: 'Montserrat_600SemiBold';
+  color: #5c8599;
+  line-height: 24px;
+  margin-top: 16px;
+`;
+
+export const MapContainer = styled.View`
+  border-radius: 20px;
+  overflow: hidden;
+  border-width: 1.2px;
+  border-color: #b3dae2;
+  margin-top: 40px;
+  background-color: #e6f7fb;
+`;
+
+export const Map = styled(MapView)`
+  width: 100%;
+  height: 150px;
+`;
+
+export const RoutesContainer = styled.View`
+  padding: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RoutesText = styled.Text`
+  font-family: 'Montserrat_700Bold';
+  color: #0089a5;
+`;
+
+export const Separator = styled.View`
+  height: 0.8px;
+  width: 100%;
+  background-color: #d3e2e6;
+  margin: 40px 0;
+`;
+
+export const ScheduleContainer = styled.View`
+  margin-top: 24px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ScheduleItem = styled.View`
+  width: 48%;
+  padding: 20px;
+`;
+
+export const ScheduleItemBlue = styled(ScheduleItem)`
+  background-color: #e6f7fb;
+  border-width: 1px;
+  border-color: #b3dae2;
+  border-radius: 20px;
+`;
+
+export const ScheduleItemGreen = styled(ScheduleItem)`
+  background-color: #edfff6;
+  border-width: 1px;
+  border-color: #a1e9c5;
+  border-radius: 20px;
+`;
+
+export const ScheduleItemRed = styled(ScheduleItem)`
+  background-color: #fef6f9;
+  border-width: 1px;
+  border-color: #ffbcd4;
+  border-radius: 20px;
+`;
+
+export const ScheduleText = styled.Text`
+  font-family: 'Montserrat_600SemiBold';
+  font-size: 16px;
+  line-height: 24px;
+  margin-top: 20px;
+`;
+
+export const ScheduleTextBlue = styled(ScheduleText)`
+  color: #5c8599;
+`;
+
+export const ScheduleTextGreen = styled(ScheduleText)`
+  color: #37c77f;
+`;
+
+export const ScheduleTextRed = styled(ScheduleText)`
+  color: #ff669d;
+`;
+
+export const ContactButton = styled(RectButton)`
+  background-color: #3cdc8c;
+  border-radius: 20px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  margin-top: 40px;
+`;
+
+export const ContactButtonText = styled.Text`
+  font-family: 'Montserrat_800ExtraBold';
+  color: #fff;
+  font-size: 16px;
+  margin-left: 16px;
+`;
